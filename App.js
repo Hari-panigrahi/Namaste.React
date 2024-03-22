06b13b03a4c94f75925e89e363ac0790a5177ac2
Hari-panigrahi <45Pharii@gmail.com>
2024-03-22 22:26:53 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,28 @@
+{/* <div id="parent">
+    <div id="child">
+        <h1> I'm a h1 tag!</h1>
+        <h2>I'm a h2 tag!</h2>
+    </div>
+</div>
+
+
+ReactElement(Object) => HTML(Browser Understands) */}
+
+const parent = react.createElement(
+    "div",
+    {id:"parent"},
+    react.createElement(
+        "div",
+        {id:"child"},[
+        react.createElement("h1",{},"I'm a h1 tag!"),
+        react.createElement("h2",{},"I'm a h2 tag!"),
+    ]
+    )
+);
+
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
